56affe216c11c7e96c2d372cb0f5c4c2cc94b8b8
Nick Sullivan <nbsullivan@gmail.com>
2015-11-13 15:02:29 -0800

    code refactored into questions and powerfit model fixed also removed some unneeded plots

--- Curvefitting/austinproject.R
+++ Curvefitting/austinproject.R
@@ -3,33 +3,21 @@ library(ggplot2)
 getwd()
 setwd("C:/Users/nick/Documents/GitHub/Math489/CurveFitting")
 
-
+#attaching data
 austin <- read.csv("ausmodeldata.csv")
 austintest <- read.csv("austestdata.csv")
 austintest$Status <- "Actual"
 austin$Status <- "Actual"
 
-austinln <- austin
-austinln$Population <- log(austinln$Population)
-austinln$Date <- log(austinln$Date)
+#Q1
 
-# intital plots of austin population for years 1850-1960
+#plot of data from 1850 to 1950
 ggplot(data = austin, aes(x = Date, y = Population)) +
   geom_point() +
   geom_line() +
   ggtitle("Austin Population")
 
-ggplot(data = austin, aes(x = Date, y = log(Population))) +
-  geom_point() +
-  geom_line() +
-  ggtitle("Austin Log(Population)")
-
-# bulidng models for population growth
-lmfit <- lm(Population ~ Date, data = austin)
-quadfit <- lm(Population ~ Date + I(Date^2), data = austin)
-expfit <- lm(log(Population) ~ Date, data = austin)
-powerfit <- lm(Population ~ Date, data = austinln)
-
+#plot of data with linear fit
 ggplot(data = austin, aes(x = Date, y = Population)) +
   geom_point() +
   stat_smooth(method = "lm", formula = y~x) +
@@ -37,82 +25,27 @@ ggplot(data = austin, aes(x = Date, y = Population)) +
 
 ggsave("austestlin.jpeg")
 
-ggplot(data = austin, aes(x = Date, y = Population)) +
-  geom_point() +
-  stat_smooth(method = "lm", formula = y~ x + I(x^2)) +
-  ggtitle("Austin Population With quadraic model")
+#creating linear model for data 1850-1950
+lmfit <- lm(Population ~ Date, data = austin)
 
-ggsave("austestquad.jpeg")
-
-
-ggplot(data = austin, aes(x = log(Date), y = log(Population))) +
-  geom_point() +
-  stat_smooth(method = "lm", formula = y~ x) +
-  ggtitle("Austin Population With log model")
-
-ggsave("austestlog.jpeg")
-
-
-# setting up dataframes for making predictions
+#make a dataframe to put in the predictions
 auslm <- data.frame(Date = c(1960,1970,1980,1990,2000,2010))
-ausquad <- data.frame(Date = c(1960,1970,1980,1990,2000,2010))
-ausexp <- data.frame(Date = c(1960,1970,1980,1990,2000,2010))
-auspower <- data.frame(Date = log(c(1960,1970,1980,1990,2000,2010)))
 
-#making predictions for each model
 auslm$Population <- predict(lmfit, newdata = auslm)
-ausquad$Population <- predict(quadfit, newdata = ausquad)
-ausexp$Population <- exp(predict(expfit, newdata = ausexp))
-auspower$Population <- exp(predict(powerfit, newdata = auspower))
 
-#changing auspower$Date back to years
-auspower$Date <- exp(auspower$Date)
-
-#denoting the entires as predicted
 auslm$Status <- "Predicted"
-ausquad$Status <- "Predicted"
-ausexp$Status <- "Predicted"
-auspower$Status <- "Predicted"
 
-#appending together results of predictions and actaul populations
 auslmtotal <- rbind(auslm, austintest, austin)
-ausquadtotal <- rbind(ausquad, austintest, austin)
-ausexptotal <- rbind(ausexp, austintest, austin)
-auspowertotal <- rbind(auspower, austintest, austin)
 
-#colorized plots of prediction models
+#ploting predictions against actaul data
 ggplot(data = auslmtotal, aes(x = Date, y = Population, color = Status)) +
   geom_line() +
   geom_point() +
   ggtitle("Austin population with linear model")
 ggsave("auscolorlm.jpeg")
 
-ggplot(data = ausquadtotal, aes(x = Date, y = Population, color = Status)) +
-  geom_line() +
-  geom_point() +
-  ggtitle("Austin population with quadardic model")
-ggsave("auscolorquad.jpeg")
-
-ggplot(data = ausexptotal, aes(x = Date, y = Population, color = Status)) +
-  geom_line() +
-  geom_point() +
-  ggtitle("Austin population with exponential model")
-ggsave("auscolorexp.jpeg")
-
-ggplot(data = auspowertotal, aes(x = Date, y = Population, color = Status)) +
-  geom_line() +
-  geom_point() +
-  ggtitle("Austin population with power-law fit")
-ggsave("auscolorpower.jpeg")
-
-#other plots
-ggplot(data = austin, aes(x = Date, y = Population)) +
-  geom_point() +
-  stat_smooth(method = "lm", formula = y~x) +
-  ggtitle("Austin Population With linear model")
-
-ggsave("austestlin.jpeg")
-
+#Q2
+#plot of model data with quad fit on it
 ggplot(data = austin, aes(x = Date, y = Population)) +
   geom_point() +
   stat_smooth(method = "lm", formula = y~ x + I(x^2)) +
@@ -120,11 +53,89 @@ ggplot(data = austin, aes(x = Date, y = Population)) +
 
 ggsave("austestquad.jpeg")
 
+#creating model for quadraic fit
+quadfit <- lm(Population ~ Date + I(Date^2), data = austin)
 
-ggplot(data = austin, aes(x = log(Date), y = log(Population))) +
+#predicting using model
+ausquad <- data.frame(Date = c(1960,1970,1980,1990,2000,2010))
+
+ausquad$Population <- predict(quadfit, newdata = ausquad)
+
+ausquad$Status <- "Predicted"
+
+ausquadtotal <- rbind(ausquad, austintest, austin)
+
+#ploting predicted v. actaul data
+ggplot(data = ausquadtotal, aes(x = Date, y = Population, color = Status)) +
+  geom_line() +
   geom_point() +
-  stat_smooth(method = "lm", formula = y~ x) +
-  ggtitle("Austin Population With log model")
+  ggtitle("Austin population with quadardic model")
+ggsave("auscolorquad.jpeg")
 
-ggsave("austestlog.jpeg")
+#Q3
+#plot of ln pop v. ln time since founding (1839)
+#creating dataframes with ln time since founding
+austinln <- austin
+austinln$Date <- log(austinln$Date - 1839)
+austinln$Population <- log(austinln$Population)
 
+austintestln <- austintest
+austintestln$Date <- log(austintestln$Date -1839)
+austintestln$Population <- log(austintestln$Population)
+
+#ploting austinln data
+ggplot(data = austinln, aes(x = Date, y = Population)) +
+  geom_point() +
+  geom_line() +
+  ggtitle("Austin ln(Population) v. ln(years since founding)")
+
+#creating model for powerlaw fit
+powerfit <- lm(Population ~ Date, data = austinln)
+
+#predicting powerlaw fit
+auspower <- data.frame(Date = log(c(1960,1970,1980,1990,2000,2010) - 1850))
+
+auspower$Population <- predict(powerfit, newdata = auspower)
+
+auspower$Status <- "Predicted"
+
+auspowertotal <- rbind(auspower, austinln, austintestln)
+
+#ploting predictions v. actaul data
+ggplot(data = auspowertotal, aes(x = Date, y = Population, color = Status)) +
+  geom_line() +
+  geom_point() +
+  ggtitle("Austin log(population) v. log(date since founding) with power-law fit")
+ggsave("auscolorpower.jpeg")
+
+#Q4
+#creating dataframes with ln pop
+austinpopln <- austin
+austintestpopln <- austintest
+
+austinpopln$Population <- log(austinpopln$Population)
+austintestpopln$Population <- log(austintestpopln$Population)
+
+#plot ln population v. time since founding
+ggplot(data = austin, aes(x = Date, y = log(Population))) +
+  geom_point() +
+  geom_line() +
+  ggtitle("Austin Log(Population)")
+
+#bulid model for exponential fit
+expfit <- lm(log(Population) ~ Date, data = austin)
+
+#making predictions for each model
+ausexp <- data.frame(Date = c(1960,1970,1980,1990,2000,2010))
+
+ausexp$Population <- predict(expfit, newdata = ausexp)
+ausexp$Status <- "Predicted"
+
+ausexptotal <- rbind(ausexp, austinpopln, austintestpopln)
+
+#ploting exponential fit
+ggplot(data = ausexptotal, aes(x = Date, y = Population, color = Status)) +
+  geom_line() +
+  geom_point() +
+  ggtitle("Austin population with exponential model")
+ggsave("auscolorexp.jpeg")
